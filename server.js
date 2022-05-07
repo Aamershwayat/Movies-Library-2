6b4f52c8f46ef17e5c1b6645cd2c7f86a7cb4f17
Aamershwayat <amerabdalhameed77@gmail.com>
2022-05-07 22:48:59 +0300

    task 12

--- server.js
+++ server.js
@@ -1,60 +1,109 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const cors= require('cors')
-const app = express(); 
-const jsonData = require("./MovieData/data.json");
-    
-    var error_404 = {
-        "status": 404,
-        "responseText": "This page endpoint not found "
-        }
-
-    var error_500 =  {
-            "status": 500,
-            "responseText": "Sorry, something went wrong"
-            }
-
-// www.localhost:3000/hello 
-
-app.get ('/' , homepage )
-app.get ('/favorite', favorite)
-app.get('*', error404 )
-app.get('*', error500 )
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const dotenv = require('dotenv');
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
+dotenv.config();
 
 
-function homepage(req,res){
-    res.status(200).json({
-        "title": "Spider-Man: No Way Home",
-        "poster_path": "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
-        "overview": "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man."
+
+
+app.get("/", (req, res) => {
+    return res.status(200).send("Hello World");
+});
+app.get("/trending", trendingHandler);
+app.get("/upcoming", upcomingMovie)
+app.get("/search", searchMovieHandler)
+app.get("/recommendations", recommended)
+app.use(errorHandler);
+app.use("*", notFountHandler);
+
+
+
+
+
+function Movie(title, id, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
+function trendingHandler(req, res) {
+    let movies = []
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value => {
+        value.data.results.forEach(element => {
+            let movie = new Movie(element.title, element.id, element.release_date, element.poster_path, element.overview);
+            movies.push(movie);
+
         })
-    }
-    
-    function Ensure(title,poster_path,overview)
-    {
-        this.title = title;
-        this.poster_path = poster_path;
-        this.overview = overview;
+
+
+        return res.status(200).json(movies);
+    }).catch((error) => {
+        errorHandler(error, req, res);
+    })
+
+};
+function searchMovieHandler(req, res) {
+
+    let serachQuery = req.query.searchStr;
+    let numberOfPages = req.query.numberOfPages
+    let movies = [];
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&language=en-US&query=${serachQuery}&page=${numberOfPages}`).then(value => {
+        value.data.results.forEach(element => {
+            let movie = new Movie(element.title, element.id, element.release_date, element.poster_path, element.overview);
+            movies.push(movie);
+        })
+        return res.status(200).json(movies);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+}
+function recommended(req, res) {
+
+    let recommendFilmId = req.query.recommendFilmId;
+    let movies = [];
+    axios.get(`https://api.themoviedb.org/3/movie/${recommendFilmId}/recommendations?api_key=${APIKEY}&language=en-US&page=1`).then(value => {
+        value.data.results.forEach(element => {
+            let recomendedMovie = new Movie(element.title, element.id, element.release_date, element.poster_path, element.overview);
+            movies.push(recomendedMovie);
+        })
+
+        return res.status(200).json(movies);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+}
+function upcomingMovie(req, res) {
+
+
+    let movies = [];
+    axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US`).then(value => {
+        value.data.results.forEach(element => {
+            let recomendedMovie = new Movie(element.title, element.id, element.release_date, element.poster_path, element.overview);
+            movies.push(recomendedMovie);
+        })
+        return res.status(200).json(movies);
+    }).catch(error => {
+        errorHandler(error, req, res);
+    })
+}
+function notFountHandler(req, res) {
+    res.status(404).send("No endpoint with this name");
+}
+function errorHandler(error, req, res) {
+    const err = {
+        status: 500,
+        message: error
     }
 
-    app.listen (3000, () => {
-        console.log("I can hear you");
-    
-    } )
-
-    function favorite(req,res){
-        res.status(200).send("Welcome to Favorite Page");
-    }
-    
-
-function error404 (req,res){
-res.status(404).send(error_404)
+    res.status(500).send(err);
 }
 
-function error500 (req,res){
-    res.status(500).send(error_500)
-    }
-    
-
-
+app.listen(PORT, () => {
+    console.log(`I am using port ${PORT}`);
+});
