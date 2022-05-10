179365496df5d0706e24d1b487dd291ce776e5b6
Aamershwayat <amerabdalhameed77@gmail.com>
2022-05-10 12:32:57 +0300

    Task13

--- server.js
+++ server.js
@@ -6,25 +6,39 @@ const axios = require("axios");
 const dotenv = require('dotenv');
 const APIKEY = process.env.APIKEY;
 const PORT = process.env.PORT;
+
+
+
 dotenv.config();
+app.use(express.json());
 
 
+// Use enviromental variable 
+const DATABASE_URL = process.env.DATABASE_URL;
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
+
+// Prepair the connnection between database  and server using pg library and database URL from .env
+const pg = require("pg");
+const client = new pg.Client(DATABASE_URL);
 
 
+// Endpoints, Routs, Path  
 app.get("/", (req, res) => {
     return res.status(200).send("Hello World");
 });
 app.get("/trending", trendingHandler);
-app.get("/upcoming", upcomingMovie)
+app.get("/upcoming",upcomingMovie)
 app.get("/search", searchMovieHandler)
-app.get("/recommendations", recommended)
+app.get("/recommendations",recommended)
+app.get("/getMovies", getFavMovieHandler);
+app.post("/addMovie", addMovieHandler);
+
+// Use methods
 app.use(errorHandler);
 app.use("*", notFountHandler);
 
-
-
-
-
+// Constructor function decleration
 function Movie(title, id, release_date, poster_path, overview) {
     this.id = id;
     this.title = title;
@@ -32,24 +46,22 @@ function Movie(title, id, release_date, poster_path, overview) {
     this.poster_path = poster_path;
     this.overview = overview;
 }
+
+// Functions declerations:
 function trendingHandler(req, res) {
     let movies = []
     axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`).then(value => {
         value.data.results.forEach(element => {
             let movie = new Movie(element.title, element.id, element.release_date, element.poster_path, element.overview);
             movies.push(movie);
-
         })
-
-
         return res.status(200).json(movies);
     }).catch((error) => {
         errorHandler(error, req, res);
     })
-
 };
-function searchMovieHandler(req, res) {
 
+function searchMovieHandler(req, res) {
     let serachQuery = req.query.searchStr;
     let numberOfPages = req.query.numberOfPages
     let movies = [];
@@ -63,8 +75,8 @@ function searchMovieHandler(req, res) {
         errorHandler(error, req, res);
     })
 }
-function recommended(req, res) {
 
+function recommended(req, res) {
     let recommendFilmId = req.query.recommendFilmId;
     let movies = [];
     axios.get(`https://api.themoviedb.org/3/movie/${recommendFilmId}/recommendations?api_key=${APIKEY}&language=en-US&page=1`).then(value => {
@@ -78,9 +90,9 @@ function recommended(req, res) {
         errorHandler(error, req, res);
     })
 }
+
+
 function upcomingMovie(req, res) {
-
-
     let movies = [];
     axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${APIKEY}&language=en-US`).then(value => {
         value.data.results.forEach(element => {
@@ -92,6 +104,35 @@ function upcomingMovie(req, res) {
         errorHandler(error, req, res);
     })
 }
+
+function getFavMovieHandler(req, res) {
+    const sql = `SELECT * FROM favariteMovie`;
+    client.query(sql)
+    .then((data) => {
+        return res.status(200).json(data.rows);
+      })
+      .catch((error) => {
+        errorHandler(error, req, res);
+      });  
+} 
+
+function addMovieHandler(req, res) {
+    let movie = req.body;
+    const sql = `INSERT INTO favariteMovie(title, release_date, poster_path, overview, comments) VALUES($1, $2, $3, $4, $5) RETURNING * ;`;
+    let values = [
+      movie.title,
+      movie.release_date,
+      movie.poster_path,
+      movie.overview,
+      movie.comments,];
+    console.log(values);
+    client.query(sql, values).then((data) => {
+        return res.status(201).json(data.rows);
+      }).catch((error) => {
+        errorHandler(error, req, res);
+      });
+}
+
 function notFountHandler(req, res) {
     res.status(404).send("No endpoint with this name");
 }
@@ -104,6 +145,9 @@ function errorHandler(error, req, res) {
     res.status(500).send(err);
 }
 
-app.listen(PORT, () => {
-    console.log(`I am using port ${PORT}`);
-});
+// Connect server to the database
+client.connect().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listen to port ${PORT}`);
+    });
+  });
